132e6ab13cdea8b46c79e9734aa0cd27014fd9e4
fanyingfx <fanyingfx@outlook.com>
2025-04-09 16:04:16 +0800

    assignement

--- compile.sh
+++ compile.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # set -x
-cfile=$@
+
+cfile=$1
 moon run src/main -- $cfile
 
 asmfile="${cfile%.c}.asm"
 
 fasm $asmfile
+
+if [[ "$2" == "run" || "$2" == "r" ]]; then
+    elf_file="${cfile%.c}"
+    eval "./${elf_file}"
+fi
