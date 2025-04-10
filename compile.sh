c1fe32fc2dc27f2fe72927cd0b26563e433d27cf
fanyingfx <fanyingfx@outlook.com>
2025-04-10 15:33:53 +0800

    update test_compiler

--- compile.sh
+++ compile.sh
@@ -4,11 +4,14 @@
 cfile=$1
 moon run src/main -- $cfile
 
-asmfile="${cfile%.c}.asm"
-
-fasm $asmfile
-
-if [[ "$2" == "run" || "$2" == "r" ]]; then
-    elf_file="${cfile%.c}"
-    eval "./${elf_file}"
+if [ $? -eq 0 ]; then
+    asmfile="${cfile%.c}.asm"
+    fasm $asmfile
+    if [[ "$2" == "run" || "$2" == "r" ]]; then
+        elf_file="${cfile%.c}"
+        eval "./${elf_file}"
+    fi
+else
+echo 'compile failed'
+exit 42
 fi
--- test_compiler.py
+++ test_compiler.py
@@ -1,26 +1,102 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-from pathlib import Path
+import shutil
 import subprocess
-example_path = Path("./examples")
-def compile_with_ccompiler(filename:str)->str:
-    cc = "clang"
-    exe_name = filename.rstrip(".c")
-    res = subprocess.run([cc,filename,"-o",exe_name])
-    if res.returncode != 0:
-        raise Exception(f"Compile with {cc} failed!")
-    return exe_name
-def compile_with_mooncompiler(filename:str)->str:
-    subprocess.run(["moon","run","src/main","--",filename],check=True)
-    asm_file = filename.replace(".c",".asm")
-    subprocess.run(["fasm",asm_file], stdout=subprocess.DEVNULL,check=True)
-    return asm_file.removesuffix(".asm")
-for file in example_path.iterdir():
-    if file.is_file() and file.name.endswith(".c"):
-        cc_exe = compile_with_ccompiler(str(file))
-        cc_res = subprocess.run([cc_exe])
-        mcc_exe = compile_with_mooncompiler(str(file))
-        mcc_res = subprocess.run([mcc_exe])
-        assert(cc_res.returncode == mcc_res.returncode)
-        print(f"{file} passed")
+from collections.abc import Callable
+from pathlib import Path
 
+BASIC_TESTS = Path("./tests/basic")
+BUILD_DIR = Path("./tests/build/")
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+RESET = "\033[0m"
+
+
+def setup_build_directory(build_path: Path) -> None:
+    """Set build folder, and cleanup"""
+    if build_path.exists():
+        if build_path.is_file():
+            build_path.unlink()
+        else:
+            shutil.rmtree(build_path)
+    build_path.mkdir()
+
+
+def cc_compile(cfile: Path) -> str:
+    """Compile with production c compiler, current is clang"""
+    cc = "clang"
+    exe_name = str(cfile.with_suffix("")) + "_cc"
+    subprocess.run([cc, str(cfile), "-o", exe_name], check=True)
+    return exe_name
+
+
+def mooncc_compile(cfile: Path) -> str:
+    """Compile with mooncc"""
+    basename = str(cfile.with_suffix(""))
+    asmfile = Path(basename + ".asm")
+
+    new_exe_name = basename + "_mcc"
+    subprocess.run(["moon", "run", "src/main", "--", str(cfile)], check=True)
+    subprocess.run(["fasm", str(asmfile)], stdout=subprocess.DEVNULL, check=True)
+
+    shutil.move(asmfile, BUILD_DIR)
+    Path(basename).rename(new_exe_name)
+    return str(new_exe_name)
+
+
+def count_cfiles(folder: Path) -> int:
+    total_count = 0
+    for file in folder.iterdir():
+        if file.is_file() and file.name.endswith(".c"):
+            total_count += 1
+    return total_count
+
+
+def compile_and_run(compile_func: Callable[[Path], str], file) -> int:
+    exe = compile_func(file)
+    res = subprocess.run([exe])
+    shutil.move(exe, BUILD_DIR)
+    return res.returncode
+
+def failed_case(folder:Path):
+    print(f"Start testing in {BLUE}{folder}{RESET}")
+    total_count = count_cfiles(folder)
+    curr_count = 0
+    for file in folder.iterdir():
+        if file.is_file() and file.name.endswith(".c"):
+            curr_count += 1
+            res = subprocess.run(["moon", "run", "src/main", "--", str(file)],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+            if res.returncode == 0:
+                print(f"{RED}{file}{RESET} not passed")
+                continue
+            print(f"{GREEN}{file.stem:<20} passed{RESET} {curr_count}/{total_count}")
+
+    print(f"End testing in {BLUE}{folder}{RESET}")
+
+
+def test_case(folder: Path):
+    print(f"Start testing in {BLUE}{folder}{RESET}")
+    total_count = count_cfiles(folder)
+    curr_count = 0
+    for file in folder.iterdir():
+        if file.is_file() and file.name.endswith(".c"):
+            curr_count += 1
+            cc_code = compile_and_run(cc_compile, file)
+            mcc_code = compile_and_run(mooncc_compile, file)
+            if cc_code != mcc_code:
+                print(f"{RED}{file}{RESET} not passed, {cc_code=}, {mcc_code=}")
+                continue
+            print(f"{GREEN}{file.stem:<20} passed{RESET} {curr_count}/{total_count}")
+    print(f"End testing in {BLUE}{folder}{RESET}")
+
+
+def main():
+    setup_build_directory(BUILD_DIR)
+    test_case(BASIC_TESTS)
+    # failed_case(BASIC_TESTS)
+
+
+if __name__ == "__main__":
+    main()
